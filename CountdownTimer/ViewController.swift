8582635703f4079ca6557f3fed34fbc33f62e091
Jonni Akesson <jonniakesson@Jonnis-MacBook-Pro.local>
2017-04-20 14:05:57 +0200

    Initial Commit

---
+++ CountdownTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountdownTimer
+//
+//  Created by Jonni Akesson on 2017-04-20.
+//  Copyright © 2017 Jonni Akesson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
